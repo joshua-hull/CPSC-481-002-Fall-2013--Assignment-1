e8f0428de1821c5c9a1a3e91ba70a70d938f77b8
Joshua Hull <hyperspace290@gmail.com>
2013-09-01 20:31:33 -0400

    Added logic to tab between text views and displaying pop-up notification.

--- CP SC 481 Assignment 1/ViewController.m
+++ CP SC 481 Assignment 1/ViewController.m
@@ -25,10 +25,19 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)doneEditingFrom:(UITextField *)sender {
+    [_toField becomeFirstResponder];
+}
+
+- (IBAction)doneEditingTo:(UITextField *)sender {
+    [_toField resignFirstResponder];
+}
+
+
 - (IBAction)goButtonPressed:(id)sender {
     NSString *fromTextContents = _fromText.text;
     NSString *toTextContents = _toField.text;
-    NSString *popUpContents  = [NSString stringWithFormat:@"%s %@ %s %@", "Getting directions from ", fromTextContents, " to ", toTextContents];
+    NSString *popUpContents  = [NSString stringWithFormat:@"%s %@ %s %@", "Getting directions from", fromTextContents, "to", toTextContents];
     
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Getting Directions"
                                                     message:popUpContents
