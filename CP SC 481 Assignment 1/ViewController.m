33ad6f5404078321a56736d8a5dfba8eb0dff811
Joshua Hull <hyperspace290@gmail.com>
2013-09-03 16:07:28 -0400

    Added Map and Cap pictures. Also synthesized the "To" and "From" text fields.

--- CP SC 481 Assignment 1/ViewController.m
+++ CP SC 481 Assignment 1/ViewController.m
@@ -14,6 +14,10 @@
 
 @implementation ViewController
 
+@synthesize fromText;
+@synthesize toField;
+@synthesize goButton;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -26,17 +30,17 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)doneEditingFrom:(UITextField *)sender {
-    [_toField becomeFirstResponder];
+    [toField becomeFirstResponder];
 }
 
 - (IBAction)doneEditingTo:(UITextField *)sender {
-    [_toField resignFirstResponder];
+    [toField resignFirstResponder];
 }
 
 
 - (IBAction)goButtonPressed:(id)sender {
-    NSString *fromTextContents = _fromText.text;
-    NSString *toTextContents = _toField.text;
+    NSString *fromTextContents = fromText.text;
+    NSString *toTextContents = toField.text;
     NSString *popUpContents  = [NSString stringWithFormat:@"%s %@ %s %@", "Getting directions from", fromTextContents, "to", toTextContents];
     
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Getting Directions"
@@ -44,6 +48,14 @@
                                                    delegate:nil
                                           cancelButtonTitle:@"Dismiss"
                                           otherButtonTitles:nil];
+    [alert show];
+}
+- (IBAction)showInfo:(UIButton *)sender {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Clemson University\nCPSC 481-002 Fall 2013\nAssignment 1"
+                                                   message:@"This application is a non-functioning mockup for a potential mapping application. This potential application will allow individuals to input their class schedule and recieve walkgin directions to their next class.\n\nThe author would like to thank and aknowledge iconarchive.org for the use of their icons.\n\nMap icon used under the CC Attribution-Noncommercial-No Derivate 3.0 liscence from http://www.iconarchive.com/artist/wwalczyszyn.html\n\nCollegate cap icon used under the GNU Lesser General Public License from http://www.iconarchive.com/artist/oxygen-icons.org.html"
+                                                  delegate:nil
+                                         cancelButtonTitle:@"Dismiss"
+                                         otherButtonTitles:nil];
     [alert show];
 }
 
